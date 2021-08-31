75247707f8f4ab191f1e65b8f86616baf4b7eded
jesusegg <jesusegg@hotmail.com>
2021-08-30 21:30:23 -0500

    finished crud

--- api/src/app.js
+++ api/src/app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-
-// require("./db.js");
-
 const server = express();
 
 server.name = "API";
@@ -14,10 +11,7 @@ server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://dogs-breeds-jesus-a6d82.web.app/"
-  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
@@ -27,8 +21,6 @@ server.use((req, res, next) => {
   next();
 });
 
-// server.use("/", routes);
-
 // Error catching endware.
 server.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
@@ -37,7 +29,5 @@ server.use((err, req, res, next) => {
   res.status(status).send(message);
 });
 server.use("/", routes);
-server.get("/", (req, res) => {
-  res.json("funcionando");
-});
+
 module.exports = server;
